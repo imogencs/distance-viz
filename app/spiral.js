60329fe5777ddd971c0b2db58bbccec4f64bd48e
imogencs <Imogen.ics1@gmail.com>
2022-07-23 15:14:16 -0700

    data is in wrong format just spiral shows up no bars

--- app/spiral.js
+++ app/spiral.js
@@ -1,3 +1,5 @@
+// adapted from http://bl.ocks.org/larsenmtl/222043d93a41d48b58d2bfa1e3d4f708 for jyalu <3
+
 var width = 500,
     height = 500,
     start = 0,
@@ -33,97 +35,96 @@ var path = svg.append("path")
     .attr("id", "spiral")
     .attr("d", spiral)
     .style("fill", "none")
-    .style("stroke", "steelblue");
+    .style("stroke", "skyblue");
 
-// fudge some data, 2 years of data starting today
-var spiralLength = path.node().getTotalLength(),
-    N = 730,
-    barWidth = (spiralLength / N) - 1;
-var someData = [];
-for (var i = 0; i < N; i++) {
-    var currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + i);
-    someData.push({
-        date: currentDate,
-        value: Math.random()
-    });
-}
 
-// here's our time scale that'll run along the spiral
-var timeScale = d3.scaleTime()
-    .domain(d3.extent(someData, function(d) {
-        return d.date;
-    }))
-    .range([0, spiralLength]);
 
-// yScale for the bar height
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(someData, function(d) {
-        return d.value;
-    })])
-    .range([0, (r / numSpirals) - 30]);
+d3.csv("https://raw.githubusercontent.com/imogencs/distance-viz/main/data/processed.csv?token=GHSAT0AAAAAABWYAQZTRMGHNCKG34X4OQGWYW4MULA", function(distanceData) {
 
-// append our rects
-svg.selectAll("rect")
-    .data(someData)
-    .enter()
-    .append("rect")
-    .attr("x", function(d, i) {
+    console.log(distanceData)
 
-        // placement calculations
-        var linePer = timeScale(d.date),
-            posOnLine = path.node().getPointAtLength(linePer),
-            angleOnLine = path.node().getPointAtLength(linePer - barWidth);
 
-        d.linePer = linePer; // % distance are on the spiral
-        d.x = posOnLine.x; // x postion on the spiral
-        d.y = posOnLine.y; // y position on the spiral
+    // TODO update length to account for different bar widths
+    var spiralLength = path.node().getTotalLength(),
+        N = 800,
+        barWidth = (spiralLength / N) - 1;
 
-        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+    // time scale that'll run along the spiral
+    var timeScale = d3.scaleTime()
+        .domain(d3.extent(distanceData, function(d) {
+            return d.date;
+        }))
+        .range([0, spiralLength]);
 
-        return d.x;
-    })
-    .attr("y", function(d) {
-        return d.y;
-    })
-    .attr("width", function(d) {
-        return barWidth;
-    })
-    .attr("height", function(d) {
-        return yScale(d.value);
-    })
-    .style("fill", "steelblue")
-    .style("stroke", "none")
-    .attr("transform", function(d) {
-        return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
-    });
+    // yScale for the bar height
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(distanceData, function(d) {
+            return d.distance;
+        })])
+        .range([0, (r / numSpirals) - 30]);
 
-// add date labels
-var tF = d3.timeFormat("%b %Y"),
-    firstInMonth = {};
-svg.selectAll("text")
-    .data(someData)
-    .enter()
-    .append("text")
-    .attr("dy", 10)
-    .style("text-anchor", "start")
-    .style("font", "10px arial")
-    .append("textPath")
-    // only add for the first of each month
-    .filter(function(d) {
-        var sd = tF(d.date);
-        if (!firstInMonth[sd]) {
-            firstInMonth[sd] = 1;
-            return true;
-        }
-        return false;
-    })
-    .text(function(d) {
-        return tF(d.date);
-    })
-    // place text along spiral
-    .attr("xlink:href", "#spiral")
-    .style("fill", "grey")
-    .attr("startOffset", function(d) {
-        return ((d.linePer / spiralLength) * 100) + "%";
-    })
+    // append rects
+    svg.selectAll("rect")
+        .data(distanceData)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+
+            // placement calculations
+            var linePer = timeScale(d.date),
+                posOnLine = path.node().getPointAtLength(linePer),
+                angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+            d.linePer = linePer; // % distance are on the spiral
+            d.x = posOnLine.x; // x postion on the spiral
+            d.y = posOnLine.y; // y position on the spiral
+
+            d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+            return d.x;
+        })
+        .attr("y", function(d) {
+            return d.y;
+        })
+        .attr("width", function(d) {
+            return barWidth;
+        })
+        .attr("height", function(d) {
+            return yScale(d.distance);
+        })
+        .style("fill", "steelblue")
+        .style("stroke", "none")
+        .attr("transform", function(d) {
+            return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+        });
+
+    // add date labels
+    var tF = d3.timeFormat("%b %Y"),
+        firstInMonth = {};
+    svg.selectAll("text")
+        .data(distanceData)
+        .enter()
+        .append("text")
+        .attr("dy", 10)
+        .style("text-anchor", "start")
+        .style("font", "10px arial")
+        .append("textPath")
+        // only add for the first of each month
+        .filter(function(d) {
+            var sd = tF(d.date);
+            if (!firstInMonth[sd]) {
+                firstInMonth[sd] = 1;
+                return true;
+            }
+            return false;
+        })
+        .text(function(d) {
+            return tF(d.date);
+        })
+        // place text along spiral
+        .attr("xlink:href", "#spiral")
+        .style("fill", "grey")
+        .attr("startOffset", function(d) {
+            return ((d.linePer / spiralLength) * 100) + "%";
+        })
+})
