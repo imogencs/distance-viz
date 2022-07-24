350898d9bc7ece182ef4649e7ac701eb49fb7467
imogencs <Imogen.ics1@gmail.com>
2022-07-24 02:44:44 -0700

    overlapping transparent squares idea

--- app/spiral.js
+++ app/spiral.js
@@ -1,20 +1,17 @@
-///////////////////////////////////
-// spiral dataviz for my darling // 
-///////////////////////////////////
-
-
-// adapted from http://bl.ocks.org/larsenmtl/222043d93a41d48b58d2bfa1e3d4f708 for jyalu <3
+// adapted from http://bl.ocks.org/larsenmtl/222043d93a41d48b58d2bfa1e3d4f708 
 
 
 //////////////////////////////////////
 // some boring constants and stuff // 
 /////////////////////////////////////
 
-var barcolor = 'lightblue'
-var imocolor = 'lightpurple'
-var jyalucolor = 'lightgreen'
+var barcolor = 'none'
+var barstrokecolor = 'black'
+var imocolor = 'red'
+var jwucolor = 'blue'
 
-var width = 1000,
+
+var width = 1200,
     height = 1000,
     start = 0,
     end = 2.25,
@@ -107,7 +104,7 @@ d3.csv("https://raw.githubusercontent.com/imogencs/distance-viz/main/data/proces
         })])
         .range([0, (r / numSpirals) - 30]);
 
-    var rect_radius = 4
+    var rect_radius = 2
 
     // append rects
     svg.selectAll("rect")
@@ -141,11 +138,118 @@ d3.csv("https://raw.githubusercontent.com/imogencs/distance-viz/main/data/proces
             return yScale(d.log_distance);
         })
         .style("fill", barcolor)
-        .style("stroke", barcolor)
+        .style("stroke", barstrokecolor)
         .style('stroke-width', 1)
         .attr("transform", function(d) {
             return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
         });
+
+
+    // append jwu rects
+    svg.selectAll("jwurect")
+        .data(distanceData)
+        .enter()
+        .append("rect")
+        .attr('rx', rect_radius)
+        .attr('ry', rect_radius)
+        .attr("x", function(d, i) {
+
+            // placement calculations
+            var linePer = timeScale(d.date),
+                posOnLine = path.node().getPointAtLength(linePer),
+                angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+            d.linePer = linePer; // % distance are on the spiral
+            d.x = posOnLine.x; // x postion on the spiral
+            d.y = posOnLine.y; // y position on the spiral
+
+            d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+            return d.x;
+        })
+        .attr("y", function(d) {
+            return d.y;
+        })
+        .attr("width", function(d) {
+            return barWidth;
+        })
+        .attr("height", function(d) {
+            return barWidth;
+        })
+        .style("fill", jwucolor)
+        .style("stroke", barcolor)
+        .style('stroke-width', 1)
+        .style('opacity', .3)
+        .attr("transform", function(d) {
+            return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+        })
+        .attr('id', 'jwurect');
+
+    // // append jwu circles
+    // svg.selectAll("jwucircle")
+    //     .data(distanceData)
+    //     .enter()
+    //     .append("circle")
+    //     .attr('r', function(d) {
+    //         return barWidth / 2;
+    //     })
+    //     .attr("x", function(d, i) {
+
+    //         // placement calculations
+    //         var linePer = timeScale(d.date),
+    //             posOnLine = path.node().getPointAtLength(linePer),
+    //             angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+    //         d.linePer = linePer; // % distance are on the spiral
+    //         d.x = posOnLine.x; // x postion on the spiral
+    //         d.y = posOnLine.y; // y position on the spiral
+
+    //         d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+    //         return d.x;
+    //     })
+    //     .attr("y", function(d) {
+    //         return d.y;
+    //     })
+    //     .style("fill", 'red')
+    //     .style('opacity', .5)
+    //     // .style("stroke", barcolor)
+    //     // .style('stroke-width', 1)
+    //     .attr("transform", function(d) {
+    //         return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+    //     })
+    //     .attr('id', 'jwucircle');
+
+    // // append imo circles
+    // svg.selectAll("imocircle")
+    //     .data(distanceData)
+    //     .enter()
+    //     .append("rect")
+    //     .attr('rx', rect_radius)
+    //     .attr('ry', rect_radius)
+    //     .attr("x", function(d, i) {
+
+    //         // placement calculations
+    //         var linePer = timeScale(d.date),
+    //             posOnLine = path.node().getPointAtLength(linePer),
+    //             angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+    //         d.linePer = linePer; // % distance are on the spiral
+    //         d.x = posOnLine.x; // x postion on the spiral
+    //         d.y = posOnLine.y; // y position on the spiral
+
+    //         d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+    //         return d.x;
+    //     })
+    //     .attr("y", function(d) {
+    //         return d.y;
+    //     })
+    //     .attr("height", function(d) {
+    //         return yScale(d.log_distance);
+    //     })
+    //     .style("fill", barcolor)
+    //     .attr('id', 'imocircle');
 
     // add date labels
     var tF = d3.timeFormat("%b %Y"),
